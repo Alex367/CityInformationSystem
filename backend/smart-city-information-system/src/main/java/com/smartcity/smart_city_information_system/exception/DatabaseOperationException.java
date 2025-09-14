24005effa271b75613ed96a8cb7c2c035a5a003a
Alex367 <liskovss1@gmail.com>
2025-09-14 22:11:24 +0200

    exception handling p.3

---
+++ backend/smart-city-information-system/src/main/java/com/smartcity/smart_city_information_system/exception/DatabaseOperationException.java
@@ -0,0 +1,7 @@
+package com.smartcity.smart_city_information_system.exception;
+
+public class DatabaseOperationException extends RuntimeException {
+  public DatabaseOperationException(String message) {
+    super(message);
+  }
+}
---
+++ backend/smart-city-information-system/src/main/java/com/smartcity/smart_city_information_system/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.smartcity.smart_city_information_system.exception;
+
+public class UnauthorizedException extends RuntimeException {
+  public UnauthorizedException(String message) {
+    super(message);
+  }
+}
