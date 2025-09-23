9852e45d8b1732e7f0feaede63d2dd6ff375dacd
Alex367 <liskovss1@gmail.com>
2025-09-23 16:44:43 +0200

    exception handling p.6

---
+++ backend/smart-city-information-system/src/main/java/com/smartcity/smart_city_information_system/exception/InvalidInputException.java
@@ -0,0 +1,7 @@
+package com.smartcity.smart_city_information_system.exception;
+
+public class InvalidInputException extends RuntimeException {
+  public InvalidInputException(String message) {
+    super(message);
+  }
+}
